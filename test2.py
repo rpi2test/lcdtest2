9bf903579eb486ef17afe14c2f65ee5f19fc37af
rpi2test <rpi2test@example.com>
2026-02-21 13:45:08 +0900

    test

--- test2.py
+++ test2.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
-import sys
-
-from PIL import Image
 import st7789
 
-disp = st7789.ST7789(
-    height=240,
-    rotation=90,
+display = st7789.ST7789(
     port=0,
-    cs=st7789.BG_SPI_CS_FRONT,  # BG_SPI_CS_BACK or BG_SPI_CS_FRONT
-    dc=9,
-    backlight=19,  # Breakout Garden: 18 for back slot, 19 for front slot.
-                    # NOTE: Change this to 13 for Pirate Audio boards
+    cs=0,
+    dc=25,
+    backlight=24,
+    rst=8,
     spi_speed_hz=80 * 1000 * 1000,
+    width=240,
+    height=240,
     offset_left=0,
-    offset_top=0,
+    offset_top=0
 )
+
+# Init and test
+display.init()
+display.fill(0x0000)  # Black
+display.text("GMT130 OK!", 10, 10, 0xFFFF)  # White text
