e76ab6c947e9e1dfc0c4537630ef98691a421c6d
rpi2test <rpi2test@misonegi.com>
2026-03-14 21:08:00 +0900

    test

--- test3.py
+++ test3.py
@@ -22,14 +22,14 @@ FONT_NOTO = ImageFont.truetype("NotoSansCJK-Regular.ttc", 48)
 COLOR_ORANGE = (255, 167, 38)
 
 # Open image file
-image = Image.open("whosnext.jpg")
+# image = Image.open("whosnext.jpg")
 
 # Resize to screen size
-image = image.resize((disp.width, disp.height), resample=Image.LANCZOS)
+# image = image.resize((disp.width, disp.height), resample=Image.LANCZOS)
 
 ###
 # Create an image with black background
-image = Image.new("RGB", (disp.width, disp.height), (0, 0, 0))
+image = Image.new("RGB", (disp.width, disp.height), (0, 50, 0))
 # Draw some text
 draw = ImageDraw.Draw(image)
 draw.text((0, 0), "みかんの", font=FONT_NOTO, fill=COLOR_ORANGE)
