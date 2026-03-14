2626d5988f7d3efb225673d23b73c6eea8bf2af2
rpi2test <rpi2test@misonegi.com>
2026-03-14 21:02:48 +0900

    ttf test

--- test3.py
+++ test3.py
@@ -4,7 +4,7 @@
 # https://www.mikan-tech.net/entry/raspi-st7789-lcd
 
 import st7789
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 
 # Create a display instance
 disp = st7789.ST7789(port=0, cs=0, rst=5, dc=6, backlight=None, spi_speed_hz=80 * 1000 * 1000)
@@ -14,12 +14,22 @@ disp._spi.mode = 3
 disp.reset()
 disp._init()
 
+# Define fonts
+FONT_ROBOTO = ImageFont.truetype("Roboto-Medium.ttf", 24)
+FONT_NOTO = ImageFont.truetype("NotoSansCJK-Regular.ttc", 48)
+
+# Define colors
+COLOR_ORANGE = (255, 167, 38)
+
 # Open image file
 image = Image.open("whosnext.jpg")
 
 # Resize to screen size
 image = image.resize((disp.width, disp.height), resample=Image.LANCZOS)
 
+# Draw some text
+draw = ImageDraw.Draw(image)
+draw.text((0, 0), "みかんの", font=FONT_NOTO, fill=COLOR_ORANGE)
+
 # Show it on display
 disp.display(image)
-# end of the list
